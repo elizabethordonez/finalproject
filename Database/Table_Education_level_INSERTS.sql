f82b017c96828c7a61619022b07f6af83ee60cd4
elizabethordonez <elizabeth_ordonez@hotmail.com>
2019-05-14 09:58:56 -0400

    organize repository, create Database file

---
+++ Database/Table_Education_level_INSERTS.sql
@@ -0,0 +1,6 @@
+INSERT INTO education_level (education_id, education_level_desc) VALUES (1, 'High School');
+INSERT INTO education_level (education_id, education_level_desc) VALUES (2, 'Associate''s degree, 2 years');
+INSERT INTO education_level (education_id, education_level_desc) VALUES (3, 'Bachelor''s degree, 4 years');
+INSERT INTO education_level (education_id, education_level_desc) VALUES (4, 'Master''s degree, 1-2 years');
+INSERT INTO education_level (education_id, education_level_desc) VALUES (5, 'Doctoral degree, 5-7 years');
+
