cfc84d5d8a35f3167ca139c2011b2ddde42ee830
elizabethordonez <elizabeth_ordonez@hotmail.com>
2019-05-15 19:01:36 -0400

    organize main repository

--- Table_Money_scale_INSERTS.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO money_scale (money_scale_id, minimum_quantity, maximum_quantity) VALUES (1,0,30000);
-INSERT INTO money_scale (money_scale_id, minimum_quantity, maximum_quantity) VALUES (2,30000,35000);
-INSERT INTO money_scale (money_scale_id, minimum_quantity, maximum_quantity) VALUES (3,35000,40000);
-INSERT INTO money_scale (money_scale_id, minimum_quantity, maximum_quantity) VALUES (4,40000,45000);
-INSERT INTO money_scale (money_scale_id, minimum_quantity, maximum_quantity) VALUES (5,45000,50000);
-INSERT INTO money_scale (money_scale_id, minimum_quantity, maximum_quantity) VALUES (6,50000,100000);
-
