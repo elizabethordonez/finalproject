913da830a2ca9ad08c2c254c4503402ca8596fec
elizabethordonez <elizabeth_ordonez@hotmail.com>
2019-05-15 17:49:20 -0400

    Datepicker for date of bith

--- Project/inputform.php
+++ Project/inputform.php
@@ -9,8 +9,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="bootstrap-3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!--  jQuery -->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 
+	<!-- Isolated Version of Bootstrap, not needed if your site already uses Bootstrap -->
+	<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
 
+	<!-- Bootstrap Date-Picker Plugin -->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,16 +36,16 @@
 				<form>
 					<div class="form-row">
 						<div class="form-group col-xs-4">
-						  <label for="inputFirstName">First Name</label>
-						  <input type="text" class="form-control" maxlength="30" id="inputFirstName" placeholder="First Name">
+						  <label for="first_name">First Name</label>
+						  <input type="text" name="first_name" class="form-control" maxlength="30" id="first_name" placeholder="First Name">
 						</div>
 						<div class="form-group col-xs-4">
-						  <label for="inputLastName">Last Name</label>
-						  <input type="text" class="form-control" id="inputLastName" placeholder="Last Name">
+						  <label for="last_name">Last Name</label>
+						  <input type="text" name="last_name" class="form-control" id="last_name" placeholder="Last Name">
 						</div>
 						<div class="form-group col-md-4">
-							<label for="inputState">Gender</label>
-							<select id="inputState" class="form-control">
+							<label for="gender">Gender</label>
+							<select id="gender" class="form-control">
 								<option>Male</option>
 								<option>Female</option>
 							</select>
@@ -45,17 +53,32 @@
 					</div>
 				
 					<div class="form-row">
-						<div class="form-group col-xs-3">
-						  <label for="dateBirth">Date of Birth</label>
-						  <input type="text" class="form-control" id="inputDateBirth" placeholder="Date of Birth">
+						<div class="form-group col-xs-2">
+						  <label for="date_of_birth">Date of Birth</label>
+							<script>
+								$(document).ready(function(){
+								  var date_input=$('input[name="date_of_birth"]'); //our date input has the name "inputDateBirth"
+								  // var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+								  var options={
+									format: 'dd/mm/yyyy',
+									// container: container,
+									todayHighlight: true,
+									autoclose: true,
+								  };
+								  date_input.datepicker(options);
+								})
+							</script>
+							
+						  <input type="text" class="form-control" id="date_of_birth" name="date_of_birth" placeholder="DD/MM/YYYY">
 						</div>
 						<div class="form-group col-md-6">
-							<label for="inputEmai">Email</label>
-							<input type="email" class="form-control" maxlength="50" id="inputEmai" placeholder="Email">
+							<label for="inputEmail4">Email</label>
+							<input type="email" class="form-control" maxlength="50" id="inputEmail4" placeholder="Email">
 						</div>
-						<div class="form-group col-xs-3">
-						  <label for="inputPhone">Phone number</label>
-						  <input type="text" class="form-control" id="inputPhone" placeholder="Phone">
+
+						<div class="form-group col-xs-4">
+						  <label for="phone">Phone number</label>
+						  <input type="text" name="phone" class="form-control" id="ghone" placeholder="Phone">
 						 </div>
 					</div>
 					
@@ -64,8 +87,8 @@
 
 					<div class="form-row">
 						<div class="form-group col-md-4">
-							<label for="inputCitizen">Country of citizenship</label>
-								<select name="countryName" class="form-control">
+							<label for="citizenship">Country of citizenship</label>
+								<select name="citizenship" class="form-control">
 									<?php
 										$row;
 										$STH = $DBH->query('SELECT country_name FROM country;');
@@ -77,8 +100,8 @@
 								</select>
 						</div>
 						<div class="form-group col-md-4">
-							<label for="inputResidence">Country of residence</label>
-								<select name="countryName" class="form-control">
+							<label for="residence">Country of residence</label>
+								<select name="residence" class="form-control">
 									<?php
 										$row;
 										$STH = $DBH->query('SELECT country_name FROM country;');
@@ -90,7 +113,7 @@
 								</select>
 						</div>
 						<div class="form-group col-md-4">
-							<label for="inputLanguage">First language</label>
+							<label for="language">First language</label>
 								<select name="language" class="form-control">
 									<?php
 										$row;
@@ -105,8 +128,8 @@
 					</div>
   					<div class="form-row">
 						<div class="form-group col-md-4">
-							<label for="inputCivil">Civil status</label>
-								<select name="civilStatus" class="form-control">
+							<label for="civil_status">Civil status</label>
+								<select name="civil_status" class="form-control">
 									<?php
 										$row;
 										$STH = $DBH->query('SELECT civil_status_desc FROM civil_status;');
@@ -118,8 +141,8 @@
 								</select>
 						</div> 
 						<div class="form-group col-md-4">
-							<label for="inputCivil">Highest level of education</label>
-								<select name="levelEducation" class="form-control">
+							<label for="education">Highest level of education</label>
+								<select name="education" class="form-control">
 									<?php
 										$row;
 										$STH = $DBH->query('SELECT education_level_desc FROM education_level;');
@@ -133,15 +156,15 @@
 					</div>
   					<div class="form-row">
 						<div class="form-group col-md-4">
-							<label for="inputIelts">Do you have an IELTS 6.5</label>
-							<select id="inputIelts" class="form-control">
+							<label for="ielts_65">Do you have an IELTS 6.5?</label>
+							<select id="ielts_65" class="form-control">
 								<option>Y</option>
 								<option>N</option>
 							</select>
 						</div>
 						<div class="form-group col-md-4">
-							<label for="inputCivil">Annual budget to come to Canada</label>
-								<select name="levelMoney" class="form-control">
+							<label for="money_scale">Annual budget to come to Canada</label>
+								<select name="money_scale" class="form-control">
 									<?php
 										$row;
 										$STH = $DBH->query('SELECT minimum_quantity, maximum_quantity FROM money_scale;');
@@ -155,9 +178,10 @@
 						<div class="form-group col-md-4">
 						</div>
 					</div>	
-
-				  <button type="submit" class="btn btn-primary">Summit</button>
+					
+				  
 			</form>
+			<button type="submit" name="submit" class="btn btn-primary">Summit</button>
 		</div>
 		</div>
 	</div>
