751598e0921bd39215115e781aa2c62fc967d618
eduardo <eduardoamador@gmail.com>
2019-05-17 16:00:18 -0400

    may17

---
+++ Project/footer.php
@@ -0,0 +1,8 @@
+<?php
+<footer class="footer">
+    <div class="container">
+    	<p class="text-center">Web Developping Team: Elizabeth Fernandez - Carolina Cadena - Eduardo Amador</p>
+      <p>Copyright (c) 2019</p>
+    </div>
+</footer>
+?>
