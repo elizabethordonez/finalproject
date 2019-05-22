1627a76fa2e6ee6e7e6549402e281809c5f9c4fd
elizabethordonez <elizabeth_ordonez@hotmail.com>
2019-05-22 00:36:10 -0400

    reorganize repository

--- Project/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-<footer class="footer">
-    <div class="container">
-    	<p class="text-center">Web Developping Team: Elizabeth Fernandez - Carolina Cadena - Eduardo Amador</p>
-      <p>Copyright (c) 2019</p>
-    </div>
-</footer>
-?>
